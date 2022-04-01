24a71c94bd8fb98e3715642ccb4a2646d3e6a0e9
Dawson Filion <Dawsonfilion21@gmail.com>
2022-04-01 17:44:45 -0400

    start javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,21 @@
-var apiKeyPolygon = "OVeW3yA0UgwO9aBeWU0HvkLBYmVeJ3Ev"
+var getStockData = function(stockName) {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com',
+            'X-RapidAPI-Key': '4a78c1e05dmsh2841aa9a1e9a1e3p1c7bb1jsn2eca213d0c16'
+        }
+    };
 
-var getCryptoData = function() {
-    var apiUrl = "https://api.polygon.io/v1/open-close/crypto/BTC/USD/2022-03-31?adjusted=true&apiKey=OVeW3yA0UgwO9aBeWU0HvkLBYmVeJ3Ev"
-
-    fetch(apiUrl)
+    fetch("https://yh-finance.p.rapidapi.com/auto-complete?q=" + stockName +"&region=US", options)
         .then(function(response){
             response.json().then(function(data) {
                 console.log(data)
             })
         })
 }
-getCryptoData()
+
+$('#searchStockBtn').on('click', function() {
+    var searchedStock = $('#searchStockInput').val();
+    getStockData(searchedStock); 
+})
