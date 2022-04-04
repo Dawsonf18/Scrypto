f6d79c7b80246c033778dd8df24ff43369556555
Gerard Mennella <gerardmennella21@gmail.com>
2022-04-04 14:13:33 -0400

    Sample structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ axios.request(options).then(function (response) {
 
     var options = {
         method: 'GET',
-        url: "https://yfapi.net/v8/finance/chart/" + stockSymbol + "?range=1mo&region=US&interval=1d&lang=en&events=div%2Csplit",
+        url: "https://yfapi.net/v8/finance/chart/" + stockSymbol + "?range=5d&region=US&interval=1d&lang=en&events=div%2Csplit",
         headers: {
           'x-api-key': 'oRR9sOAR2w9p3NQiFl5fS5A5jwP2FS0k9A033nLd'
         }
@@ -34,3 +34,6 @@ $("#searchStockBtn").on("click", function() {
     var searchedStock = $("#searchStockInput").val()
     getStockData(searchedStock)    
 })
+
+var testDate = new Date(1648843204 * 1000)
+console.log(testDate)
